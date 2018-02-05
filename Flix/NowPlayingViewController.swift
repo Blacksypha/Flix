bdc798ee836e5dc3f9b672aa00ed5ea467c0d7da
Tevin Lewis <tevinlewis@Tevins-MacBook-Pro.local>
2018-02-04 22:04:09 -0500

    Set Poster Image

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
@@ -54,6 +55,12 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        
+        let posterURL = URL(string: baseURLString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterURL)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
