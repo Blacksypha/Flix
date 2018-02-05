e1cd061db700b46b4b2e58768b36a3dc9b5a79c5
Tevin Lewis <tevinlewis@Tevins-MacBook-Pro.local>
2018-02-04 21:17:30 -0500

    Table View Setup

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     
     
+    @IBOutlet weak var tableView: UITableView!
     
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -29,10 +32,6 @@ class NowPlayingViewController: UIViewController {
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies {
-                    let title = movie["title"] as! String
-                    print(title)
-                }
             }
             }
         task.resume()
@@ -43,7 +42,14 @@ class NowPlayingViewController: UIViewController {
 
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
     
     
     
