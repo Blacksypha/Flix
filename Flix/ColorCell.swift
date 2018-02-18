07cd66d61e1ebb6af1864ee7856eda8f5c0d4fca
Tevin Lewis <tevinlewis@Tevins-MacBook-Pro.local>
2018-02-18 17:35:11 -0500

    Added Tab Bar

---
+++ Flix/ColorCell.swift
@@ -0,0 +1,14 @@
+//
+//  ColorCell.swift
+//  Flix
+//
+//  Created by Tevin Lewis on 2/18/18.
+//
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorLabel: UILabel!
+}
