875a4f4b52bc46da804fc11f1cc0012aae9fdf4d
Cory Dashiell <codas1@morgan.edu>
2018-02-18 20:24:05 -0500

    Added Superhero tab and collection view

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Tevin Lewis on 2/18/18.
+//
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
